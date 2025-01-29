c8ed8a6205b4c86accaa4d25f44c52d36becaef5
kbr_1218 <kbr_uni@naver.com>
2025-01-29 18:13:22 +0900

    [FIX] watch now 호출 api 매개변수 변경 & 웹소켓 동작과정 주석 추가

--- main.py
+++ main.py
@@ -16,33 +16,36 @@ def load_root():
 async def classify_user_input(websocket: WebSocket, user_id: str):
   await websocket.accept()
   try:
-    # 유효한 사용자인지 확인하는 과정 (view vectorstore에서 사용자 검색)
+    ### 1. 웹소켓 연결 시 먼저 벡터스토어에 시청 기록이 있는 사용자인지 확인 ###
     print(">>>>>>>", user_id)
     model_server_endpoint = f"{MODEL_SERVER_URL}{user_id}/api/connect"
     try:
       response = requests.post(model_server_endpoint)
+      # 사용자 확인 시 code 200 반환
       if response.status_code == 200:
         await websocket.send_json({"message": "✔️model server 연결 성공"})
+      # 사용자를 찾을 수 없다면 웹소켓 close
       else:
         await websocket.send_json({"error": f"model server return status {response.status_code}"})
         await websocket.close()
         return
+    # API 요청 중 예외 처리
     except requests.exceptions.RequestException as e:
       await websocket.send_json({"error": f"✖️model server 연결 실패: {str(e)}"})
       await websocket.close()
       return
     
-    # 채팅
+    ### 2. 웹소켓에 연결 + 사용자 확인된 상태 ###
     while True:
+      ### 2-1. 클라이언트가 웹소켓으로 데이터를 보냄 ###
       data = await websocket.receive_json()
-
-      # 클라이언트의 요청
       event = data.get("event", "")
 
-      # 사용자가 시청하기 버튼을 눌렀다면
+      ### 2-2. 사용자가 시청하기 버튼을 누름 ###
       if event == "watch_now":
+        # asset_id 가져오기
         asset_id = data.get("asset_id", "")
-        # asset_id가 없으면
+        runtime = float(data.get("runtime", 0))
         if not asset_id:
           await websocket.send_json({"error": "Asset ID 없음!"})
           continue
@@ -50,17 +53,20 @@ async def classify_user_input(websocket: WebSocket, user_id: str):
         # model server에 asset_id 전송
         watch_endpoint = f"{MODEL_SERVER_URL}{user_id}/api/watch"
         try:
-          response = requests.post(watch_endpoint, json={"asset_id": asset_id})
+          response = requests.post(watch_endpoint, json={"asset_id": asset_id, "runtime": runtime})
           if response.status_code == 200:
             await websocket.send_json({"message": "✔️시청기록 저장 완료"})
           else:
             await websocket.send_json({"error": f"시청기록 저장 실패. status code: {response.status_code}"})
         except requests.exceptions.RequestException as e:
           await websocket.send_json({"error": f"model server에 api 전송 실패: {str(e)}"})
+        continue
 
-      # 일반 채팅 처리
+      ### 2-3. 사용자가 채팅을 보냄 ###
       else:
+        # 사용자 input 가져오기
         user_input = data.get("user_input", "")
+        
         # 라우터 체인으로 사용자 입력 유형 분류
         classification_result = classification_chain.invoke({"user_input": user_input})
         user_input_type = classification_result.get("type", "일반대화")
